5341ee5f21ec87b6ae61cbce7128c5774b17fc03
Dhanish Ahamed <danishahamed.dk@gmail.com>
2024-10-02 00:53:59 +0530

    Add question front end functions completed.

--- src/Services/allAPI.js
+++ src/Services/allAPI.js
@@ -5,3 +5,8 @@ import { BASE_URL } from "./baseUrl"
 export const admloginAPI=async(users)=>{
     return await commonAPI('POST',`${BASE_URL}/admin/login`,users,"")
 }
+
+//Adding new question API
+export const addQuestionAPI=async(question)=>{
+    return await commonAPI('POST',`${BASE_URL}/general/pot/add`,question,"")
+}
